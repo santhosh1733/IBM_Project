d25ad94eaf4db396c060d5edd4832c754c11acb3
Guru2527 <guruprasadkrguru.social@gmail.com>
2026-08-12 13:06:50 +0530

    Update Bill Pay test data and fix LoginTest syntax

--- parabank-automation (1)/parabank-automation/src/test/java/com/parabank/tests/LoginTest.java
+++ parabank-automation (1)/parabank-automation/src/test/java/com/parabank/tests/LoginTest.java
@@ -7,15 +7,15 @@ import com.parabank.utils.ConfigReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class LoginTest extends BaseTest {
 
 	private static final String VALID_USERNAME = ConfigReader.get("username");
-    private static final String VALID_PASSWORD = ConfigReader.get("password");
+	private static final String VALID_PASSWORD = ConfigReader.get("password");
 
-    @Test(description = "Verify successful login with valid credentials")
-    public void verifySuccessfulLogin() {
-        AccountOverviewPage overview = new LoginPage(driver).login(VALID_USERNAME, VALID_PASSWORD);
-        Assert.assertTrue(overview.isAccountsTableDisplayed(),
-                "Expected Accounts Overview table to be displayed after login");
+	@Test(description = "Verify successful login with valid credentials")
+	public void verifySuccessfulLogin() {
+		AccountOverviewPage overview = new LoginPage(driver).login(VALID_USERNAME, VALID_PASSWORD);
+		Assert.assertTrue(overview.isAccountsTableDisplayed(),
+				"Expected Accounts Overview table to be displayed after login");
+	}
 }
