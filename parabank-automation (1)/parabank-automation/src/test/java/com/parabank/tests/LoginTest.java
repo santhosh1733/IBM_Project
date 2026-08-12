41ff1ccd675e4dfff6493891990b2beadb2b3aa1
Guru2527 <guruprasadkrguru.social@gmail.com>
2026-08-12 11:16:08 +0530

    Fix LoginTest configuration credentials

--- parabank-automation (1)/parabank-automation/src/test/java/com/parabank/tests/LoginTest.java
+++ parabank-automation (1)/parabank-automation/src/test/java/com/parabank/tests/LoginTest.java
@@ -3,19 +3,30 @@ package com.parabank.tests;
 import com.parabank.pages.AccountOverviewPage;
 import com.parabank.pages.LoginPage;
 import com.parabank.utils.ConfigReader;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
+/**
+ * Login module tests.
+ *
+ * Credentials are read from the same configuration keys used by the other
+ * modules (including BillPayTest). They can also be overridden at runtime:
+ *
+ * mvn test -Dtest=LoginTest -Dparabank.username=<user> -Dparabank.password=<pass>
+ */
 public class LoginTest extends BaseTest {
-
-	private static final String VALID_USERNAME = ConfigReader.get("username");
-    private static final String VALID_PASSWORD = ConfigReader.get("password");
 
     @Test(description = "Verify successful login with valid credentials")
     public void verifySuccessfulLogin() {
-        AccountOverviewPage overview = new LoginPage(driver).login(VALID_USERNAME, VALID_PASSWORD);
+        String username = System.getProperty(
+                "parabank.username", ConfigReader.get("parabank.username"));
+        String password = System.getProperty(
+                "parabank.password", ConfigReader.get("parabank.password"));
+
+        AccountOverviewPage overview = new LoginPage(driver).login(username, password);
+
         Assert.assertTrue(overview.isAccountsTableDisplayed(),
-                "Expected Accounts Overview table to be displayed after login");
+                "Expected Accounts Overview table to be displayed after login. "
+                        + "Check parabank.username/parabank.password in config.properties.");
+    }
 }
